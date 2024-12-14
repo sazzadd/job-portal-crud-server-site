28770076799f29362461c58cf5ff1c3476eee8a6
sazzad <sazzadador@gmail.com>
2024-12-14 12:06:59 +0600

    job portal server sit

---
+++ index.js
@@ -0,0 +1,105 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// Middleware
+app.use(cors());
+app.use(express.json());
+// user :jobportal
+// pass:ador123
+
+// MongoDB URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.krhx2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// const uri =
+//   "mongodb+srv://<db_username>:<db_password>@cluster0.krhx2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  const jobsCollection = client.db("job-portal").collection("jobs");
+  const jobApplicationCollection = client
+    .db("job-portal")
+    .collection("job_apllications");
+  try {
+    // GET job
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+    // post job
+    app.post("/jobs", async (req, res) => {
+      const newJob= req.body;
+      console.log("creating new users", newJob);
+      const result = await jobsCollection.insertOne(newJob);
+      res.send(result);
+    });
+
+    // job apllicaton apis
+    app.post("/job-applications", async (req, res) => {
+      const application = req.body;
+      console.log(application);
+      const result = await jobApplicationCollection.insertOne(application);
+      res.send(result);
+    });
+    app.get("/job-applications", async (req, res) => {
+      const email = req.query.email;
+      const query = { apllicant_email: email };
+      const cursor = jobApplicationCollection.find(query);
+      const result = await cursor.toArray();
+
+      for (const application of result) {
+        console.log(application.job_id);
+        const query1 = { _id: new ObjectId(application.job_id) };
+        const job = await jobsCollection.findOne(query1);
+        if (job) {
+          application.title = job.title;
+          application.company = job.company;
+          application.company_logo = job.company_logo;
+          application.location = job.location;
+          application.jobType = job.jobType;
+          application.category = job.category;
+        }
+      }
+
+      res.send(result);
+    });
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Job Poratal Server is Running!");
+});
+
+app.listen(port, () => {
+  console.log(`Job Poratal listening on port ${port}`);
+});
