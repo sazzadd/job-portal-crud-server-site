452b128ad5e933b76111e2d83e75f330071e3697
sazzad <sazzadador@gmail.com>
2024-12-17 11:42:58 +0600

    updated

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+var jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+const logger = (req, res, next) => {
+  console.log("inside the logger");
+  next();
+};
+const verifyToken = (req, res, next) => {
+  console.log("inside verify token midleware.cookies", req.cookies);
+  const token = req?.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ massage: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ massage: "autorized access token" });
+    }
+    //
+    req.user = decoded;
+    next();
+  });
+};
+
 // user :jobportal
 // pass:ador123
 
@@ -32,8 +59,31 @@ async function run() {
     .db("job-portal")
     .collection("job_apllications");
   try {
+    // auth realted api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", async (req, res) => {
+      res.clearCookie("token", {
+        httpOnly: true,
+        secure: false,
+      })
+      .send({ success: true });
+
+    });
+
     // GET job
-    app.get("/jobs", async (req, res) => {
+    app.get("/jobs", logger, async (req, res) => {
+      console.log("now inside the api call back");
       const email = req.query.email;
       let query = {};
       if (email) {
@@ -84,14 +134,19 @@ async function run() {
       const updateResult = await jobsCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
-    app.get("/job-applications", async (req, res) => {
+    app.get("/job-applications", verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { apllicant_email: email };
+      console.log("cookies:", req.cookies);
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({ massage: "forbidden access " });
+      }
+
       const cursor = jobApplicationCollection.find(query);
       const result = await cursor.toArray();
 
       for (const application of result) {
-        console.log(application.job_id);
+        // console.log(application.job_id);
         const query1 = { _id: new ObjectId(application.job_id) };
         const job = await jobsCollection.findOne(query1);
         if (job) {
